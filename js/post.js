dc965348d1750aa795d4cf36587f6a5dac86fe60
-10 <hushi55@126.com>
2015-01-04 14:48:44 +0800

    添加 disqus 评论，并且调整它的位置，底部从 80px 到 40px

--- js/post.js
+++ js/post.js
@@ -201,4 +201,18 @@ $(document).ready(function(){
         $("head").append("<link rel='stylesheet' type='text/css' href='/css/css3-ani.css'/>");
         $.getScript('/js/css3-ani.js',function(){});
     }
+
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'bloghushi55'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function () {
+
+    	$('#content').append('<div id="disqus_thread" class="entry"></div>');
+
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
 });
