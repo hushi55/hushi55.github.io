8cbf672853ecd2c67edf42bd94a384f2d3d520dd
hushi.hs <hushi.hs@alibaba-inc.com>
2021-04-06 17:29:43 +0800

    use gitalk

--- js/post.js
+++ js/post.js
@@ -220,24 +220,47 @@ $(document).ready(function(){
 	//<!-- 多说评论框 end -->
 
     <!-- 来必力City版安装代码 -->
+    // (function(d, s) {
+    //
+    //     var ds_div = '<div id="lv-container" data-id="city" data-uid="MTAyMC8zMTMzOC83ODg3" class="entry" style="clear:none"><noscript> 为正常使用来必力评论功能请激活JavaScript</noscript></div>';
+    //     $('#content').append(ds_div);
+    //
+    //
+    //
+    //     var j, e = d.getElementsByTagName(s)[0];
+    //
+    //     if (typeof LivereTower === 'function') { return; }
+    //
+    //     j = d.createElement(s);
+    //     j.src = 'https://cdn-city.livere.com/js/embed.dist.js';
+    //     j.async = true;
+    //
+    //     e.parentNode.insertBefore(j, e);
+    // })(document, 'script');
+    <!-- City版安装代码已完成 -->
     (function(d, s) {
 
-        var ds_div = '<div id="lv-container" data-id="city" data-uid="MTAyMC8zMTMzOC83ODg3" class="entry" style="clear:none"><noscript> 为正常使用来必力评论功能请激活JavaScript</noscript></div>';
-        $('#content').append(ds_div);
-
-
-
         var j, e = d.getElementsByTagName(s)[0];
-
         if (typeof LivereTower === 'function') { return; }
 
-        j = d.createElement(s);
-        j.src = 'https://cdn-city.livere.com/js/embed.dist.js';
-        j.async = true;
+        {
+            j = d.createElement('link');
+            j.href = 'https://unpkg.com/gitalk/dist/gitalk.css';
+            j.type = 'text/css';
+            j.rel = 'stylesheet';
+            e.parentNode.insertBefore(j, e);
+        }
 
-        e.parentNode.insertBefore(j, e);
+        {
+            j = d.createElement(s);
+            j.src = 'https://unpkg.com/gitalk/dist/gitalk.min.js';
+            j.async = true;
+            e.parentNode.insertBefore(j, e);
+        }
+
     })(document, 'script');
-    <!-- City版安装代码已完成 -->
 
 
 });
+
+
